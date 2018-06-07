d5e41a436ad589b23e9b65739222a6ed09cdb5f1
Vishwadeep1991 <40033188+Vishwadeep1991@users.noreply.github.com>
2018-06-07 23:08:12 +0530

    Add files via upload

---
+++ Example3.java
@@ -0,0 +1,12 @@
+class Example3
+{
+   public static void main(String args[]){
+	try{
+		int num=Integer.parseInt ("XYZ");
+		System.out.println(num);
+	}
+	catch(NumberFormatException e){
+	System.out.println("Number format exception occurred");
+	}
+    }
+}
---
+++ Example5.java
@@ -0,0 +1,12 @@
+class Example5
+{
+   public static void main(String args[]){
+	try{
+		String str=null;
+		System.out.println(str.length());
+	}
+	catch(NullPointerException e){
+	System.out.println("NullPointerException occurred");
+	}
+    }
+}
