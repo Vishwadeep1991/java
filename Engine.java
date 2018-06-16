b31abe738534453310aa765c8872cc53d8541e5a
Vishwadeep1991 <40033188+Vishwadeep1991@users.noreply.github.com>
2018-06-16 11:51:37 +0530

    Add files via upload

---
+++ Engine.java
@@ -0,0 +1,12 @@
+package com.Engines;
+
+public class Engine{
+	public int engine1000cc(){
+	int rpmCount=100;
+	return rpmCount;
+	}
+public int engine3000cc(){
+	int rpmCount=300;
+	return rpmCount;
+	}
+}
---
+++ Ford.java
@@ -0,0 +1,9 @@
+package com.cars;
+import com.Engines.Engine;
+
+public class Ford{
+	void moveFast(){
+	Engine e=new Engine();
+	int rpm=e.engine3000cc();
+	}
+}
