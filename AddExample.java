4fb621c89b3564b257e2b1a216016d227323aae5
Vishwadeep1991 <40033188+Vishwadeep1991@users.noreply.github.com>
2018-07-02 09:17:01 +0530

    Add files via upload

---
+++ AddExample.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+
+public class AddExample{
+ public static void main(String[] args){
+int i,j;
+for (i=0; i<=5; i++)
+{
+  for (j=0; j<=5; j++)
+{
+    LinkedList<String> linkedlist = new LinkedList<String>();
+    linkedlist.add("AM");
+    linkedlist.add("BN");
+    linkedlist.add("CO");
+    linkedlist.add("DP");
+    linkedlist.add("EQ");
+    System.out.println(linkedlist);
+    linkedlist.addFirst("FIRST");
+    System.out.println(linkedlist);
+    linkedlist.addLast("LAST");
+    System.out.println(linkedlist);
+   }
+  }
+  System.out.println("n");
+ }
+
+}
